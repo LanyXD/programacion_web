97d7b0aeea801bf2bd9358f110fa9f16eb6d2b6e
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-08-15 11:57:15 -0600

    feat: class-11

---
+++ cls-11/react-intro/src/components/Title.jsx
@@ -0,0 +1,7 @@
+function Title() {
+    return(<>
+    <h1>Hello world in react</h1>
+    </>)
+}
+
+export default Title
---
+++ cls-11/react-intro/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './components/App.jsx'
+import Title from './components/Title.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <Title />
+  </StrictMode>,
+)
