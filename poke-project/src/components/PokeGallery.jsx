24583babe701a6491f80a0f18c1f5dd6b3b6dd09
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-08-17 16:05:58 -0600

    feat: add pokeAPI

--- poke-project/src/components/PokeGallery.jsx
+++ poke-project/src/components/PokeGallery.jsx
@@ -1,6 +1,13 @@
-function PokeGallery () {
+function PokeGallery ({ pokemons }) {
     return (
-        <div className="gallery"></div>
+        <div className="gallery">
+            {pokemons.map(pokemon => (
+                <div>
+                    <h1>{pokemon.name}</h1>
+                    <img src={pokemon.sprites.front_default} alt="imagene pokmeon" />
+                </div>
+            ))}
+        </div>
     )
 }
 
