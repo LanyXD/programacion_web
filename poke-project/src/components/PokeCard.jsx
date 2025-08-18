1a50722d5b31a2a2d1e66cc3f5158596759c04cf
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-08-17 20:14:34 -0600

    style: add style for card by type

--- poke-project/src/components/PokeCard.jsx
+++ poke-project/src/components/PokeCard.jsx
@@ -2,10 +2,10 @@ function PokeCard ({ pokemon }) {
 
     return (
         <>
-        <div className="card">
+        <div className={`card ${pokemon.types[0].type.name}`}>
             <p className="name">{pokemon.name}</p>
             <img className="image" src={pokemon.sprites.front_default} alt="Image not found." />
-            <p className="type">{pokemon.type}</p>
+            <p className="type">{pokemon.types[0].type.name}</p>
         </div>
         </>
     )
