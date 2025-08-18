4aa0174129283eb953e91ca66e24fede373c14e3
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-08-17 20:39:19 -0600

    feat: pokenav working

--- poke-project/src/App.jsx
+++ poke-project/src/App.jsx
@@ -3,18 +3,19 @@ import axios from "axios"
 import PokeGallery from './components/PokeGallery'
 import PokeNav from './components/PokeNav'
 
-
 function App() {
-  const [pokemons, setPokemons] = useState([])
-  
+  const [allPokemons, setAllPokemons] = useState([])
+  const [currentPage, setCurrentPage] = useState(0)
+  const pageSize = 6
+
   useEffect(() => {
     const fetchPokemons = async () => {
       try {
-        const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=6")
+        const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=50") // trae más Pokémon
         const details = await Promise.all(
           res.data.results.map(p => axios.get(p.url).then(r => r.data))
         )
-        setPokemons(details)
+        setAllPokemons(details)
       } catch (error) {
         console.error("Error cargando Pokémon:", error)
       }
@@ -23,10 +24,24 @@ function App() {
     fetchPokemons()
   }, [])
 
+  const pagedPokemons = allPokemons.slice(currentPage * pageSize, (currentPage + 1) * pageSize)
+
+  const nextPage = () => {
+    if ((currentPage + 1) * pageSize < allPokemons.length) {
+      setCurrentPage(prev => prev + 1)
+    }
+  }
+
+  const prevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(prev => prev - 1)
+    }
+  }
+
   return (
     <>
-      <PokeGallery pokemons={pokemons} />
-      <PokeNav />
+      <PokeGallery pokemons={pagedPokemons} />
+      <PokeNav next={nextPage} prev={prevPage}/>
     </>
   )
 }
--- poke-project/src/components/PokeNav.jsx
+++ poke-project/src/components/PokeNav.jsx
@@ -1,8 +1,9 @@
-function PokeNav () {
+function PokeNav ({next, prev}) {
     return (
         <>
         <div className="navigation">
-            
+            <button onClick={prev}>Back</button>
+            <button onClick={next}>Next</button>
         </div>
         </>
     )
