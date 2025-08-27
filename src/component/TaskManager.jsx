af659b0112e282a84920910837a11dfecb18a5a8
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-08-26 21:38:28 -0600

    feat: add main TaskManager component

---
+++ src/component/TaskManager.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function TaskManager() {
+
+    return (
+        <div className="main_container">
+            <h1>Task Manager</h1>
+        </div>
+    )
+}
+
+export default TaskManager;
