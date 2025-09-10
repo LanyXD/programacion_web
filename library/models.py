147a778b4ea289db63569c334e6cf3ed10cea302
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-09-10 13:39:00 -0600

    feat: add Edition model and 0003 migration

--- library/models.py
+++ library/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +12,10 @@ class Publisher(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='publishers')
     name = models.CharField(max_length=100)
     country = models.CharField(max_length=50, blank=True)  
+
+
+class Edition(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='editions')
+    edition_number = models.PositiveIntegerField()
+    format = models.CharField(max_length=50, blank=True)
+    pages = models.PositiveIntegerField(blank=True, null=True)
