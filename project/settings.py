5e9fcf5d306de2aa87f326dcfc730e9c4a96819a
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-09-10 13:07:57 -0600

    feat: add settings configuration

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)031c(zv99b3z9b89p_6tw2!ze#$#i#6p8l6-t^si*2dnu@0ir
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'library',
 ]
 
 MIDDLEWARE = [
