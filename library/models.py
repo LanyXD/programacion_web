b8dcc4603536b09c407aa985dd2b8ce85d7ecbee
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-09-10 13:19:46 -0600

    feat: add Book model and 0001 migration

--- library/models.py
+++ library/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=20, blank=True, null=True)
+    published_date = models.DateField(blank=True, null=True)
+
