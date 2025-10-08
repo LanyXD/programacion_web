502c7d0a76f6d0540db0c18ff1ed1e162c307779
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-10-07 21:28:05 -0600

    Revert "feat: add docker-compose.yml"
    This reverts commit 93674ae2e90a20457b38874e019d1c0478baa455.

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-echo "Aplicando migraciones..."
-python manage.py migrate --noinput
-
-echo "Recolectando archivos estaticos..."
-python manage.py collectstatic --noinput
-
-echo "Iniciando servidor..."
-python manage.py runserver 0.0.0.0:8000
