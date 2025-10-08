bcdb4fd187bc3cb503a9ad3cb91720db9d6fa983
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-10-07 22:26:24 -0600

    fix: configure Django to use PostgreSQL .env

--- project/settings.py
+++ project/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
