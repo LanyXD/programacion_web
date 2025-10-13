bc5f1a552abfad5a6f1bdeb73fec5c4e900ff78e
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-10-12 22:58:40 -0600

    feat: connect with Django API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL: API_URL,
@@ -11,20 +11,34 @@ const api = axios.create({
 
 export const createLink = async (data) => {
   try {
-    const response = await api.post("/links/", data);
+    const response = await api.post("/links/create/", data);
+    console.log("Link creado:", response.data);
     return response.data;
   } catch (error) {
-    console.error("Error al crear el link:", error.response?.data || error.message);
+    if (error.response) {
+      console.error("Error en backend:", error.response.status, error.response.data);
+    } else if (error.request) {
+      console.error("No se recibió respuesta del backend:", error.request);
+    } else {
+      console.error("Error al configurar la solicitud:", error.message);
+    }
     throw error;
   }
 };
 
-export const getLink = async (id) => {
+export const revealLink = async (id) => {
   try {
-    const response = await api.get(`/links/${id}/`);
+    const response = await api.get(`/links/reveal/${id}/`);
+    console.log("Mensaje revelado:", response.data);
     return response.data;
   } catch (error) {
-    console.error("Error al obtener el link:", error.response?.data || error.message);
+    if (error.response) {
+      console.error("Error en backend:", error.response.status, error.response.data);
+    } else if (error.request) {
+      console.error("No se recibió respuesta del backend:", error.request);
+    } else {
+      console.error("Error al configurar la solicitud:", error.message);
+    }
     throw error;
   }
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Hide from './pages/Hide';
 import Reveal from './pages/Reveal';
+import "./styles/main.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
