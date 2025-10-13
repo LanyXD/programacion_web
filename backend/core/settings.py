153871516d7b1049d191863dd8f1375a743023e8
LanyXD <100395231+LanyXD@users.noreply.github.com>
2025-10-12 22:54:54 -0600

    chore: update Django settings for API connection

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'linksafe',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True # dame tiempo
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
